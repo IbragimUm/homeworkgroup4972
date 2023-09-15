f55cfe8d9a0ac30561c4b1ceb3b36628240bd554
«Ibragim» <iba.95.95@bk.ru>
2023-09-15 16:48:25 +0300

    solutionSeminarHW

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,97 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+//string inputX  = Console.ReadLine()??"0";
+//string inputY = Console.ReadLine()??"0";
+//int max = 0;
+//int x = int.Parse(inputX);
+//int y = int.Parse(inputY);
+//if(x>y){
+//max = x;
+//}
+//else
+//{
+  //  max = y;
+//}
+//Console.WriteLine(max);
+
+
+
+
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+//string inputX  = Console.ReadLine()??"0";
+//string inputY = Console.ReadLine()??"0";
+//string inputZ  = Console.ReadLine()??"0";
+//int max = 0;
+//int x = int.Parse(inputX);
+//int y = int.Parse(inputY);
+//int z = int.Parse(inputZ);
+//if(x>y){
+  //  max = x;
+    //if(max>z){
+      //  max = max;
+    //}
+    //else{
+      //  max=z;
+    //}
+//}
+//else{
+  //  max=y;
+    //if(max>z){
+      //  max=max;
+    //}
+    //else{
+      //  max=z;
+    //}
+//}
+//Console.WriteLine(max);
+
+
+
+
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+//string Num = Console.ReadLine()??"0";
+//int num = int.Parse(Num);
+//if(num%2==0){
+//    Console.WriteLine("число четное");
+//}else{
+  //  Console.WriteLine("число нечетное");
+//}
+
+
+
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// string Num = Console.ReadLine()??"0";
+// int num = int.Parse(Num);
+// int count = 0;
+// while(count<=num){
+//   if(count%2==0){
+//     Console.Write(count);
+//     Console.Write(", ");
+//     count++;
+//   }
+//   else{
+//     count++;
+//   }
+// }
